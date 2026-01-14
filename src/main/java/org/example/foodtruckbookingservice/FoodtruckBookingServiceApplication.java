3d1ed18ad4c2c5b545dd9e05efd84ad8503fb272
david <David.Blaschek@bs1traunstein.de>
2026-01-14 09:06:35 +0100

    Service Renaming

---
+++ src/main/java/org/example/foodtruckbookingservice/FoodtruckBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.foodtruckbookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtruckBookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodtruckBookingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/foodtruckbookingservice/FoodtruckBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.foodtruckbookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtruckBookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
